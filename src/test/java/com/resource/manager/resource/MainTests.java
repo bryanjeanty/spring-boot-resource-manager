4b2b44d0efd1becc5ad1c08133056ced2cfaf0d7
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-06-29 17:35:49 -0400

    pom source file updated with project scope dependencies

--- src/test/java/com/resource/manager/resource/MainTests.java
+++ src/test/java/com/resource/manager/resource/MainTests.java
@@ -1,7 +1,5 @@
 package com.resource.manager.resource;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,9 +11,6 @@ public class MainTests {
 
 	@Test
 	public void contextLoads() {
-		Main m = new Main();
-
-		assertEquals("Welcome to the resource page", m.getResourcePage(), "output must be welcome message");
 	}
 
 }
