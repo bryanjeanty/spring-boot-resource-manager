0b1c5916550e71764e0e39a6fae573c6cc4cdf6a
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-06-29 20:09:03 -0400

    account controller business logic setup

---
+++ src/main/java/com/resource/manager/resource/controller/AccountController.java
@@ -0,0 +1,67 @@
+package com.resource.manager.resource.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import com.resource.manager.resource.entity.Account;
+import com.resource.manager.resource.service.AccountService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class AccountController {
+  private final AccountService accountService;
+
+  @Autowired // <-- deviation
+  public AccountController(AccountService accountService) {
+    this.accountService = accountService;
+  }
+
+  @PostMapping("/accounts")
+  public ResponseEntity<String> createAccount(@Valid @RequestBody Account account) {
+    accountService.save(account);
+    return new ResponseEntity<>("Account successfully created", HttpStatus.CREATED);
+  }
+
+  @GetMapping("/accounts")
+  public List<Account> getAllAccounts() {
+    return accountService.findAll();
+  }
+
+  @GetMapping("/accounts/{id}")
+  public Account getAccountById(@PathVariable("id") int accountId) {
+    return accountService.findById(accountId).orElse(null);
+  }
+
+  @PutMapping("/accounts/{id}")
+  public ResponseEntity<String> updateAccountById(@PathVariable("id") int accountId,
+      @Valid @NotNull @RequestBody Account updates) {
+    Account updatedAccount = accountService.findById(accountId).orElse(null);
+    updatedAccount.setUsername(updates.getUsername());
+    updatedAccount.setEmail(updates.getEmail());
+    updatedAccount.setPassword(updates.getPassword());
+
+    accountService.save(updatedAccount);
+    return new ResponseEntity<>("Account successfully updated", HttpStatus.OK);
+  }
+
+  @DeleteMapping("/accounts/{id}")
+  public ResponseEntity<String> deleteAccountById(@PathVariable("id") int accountId) {
+    Account account = accountService.findById(accountId).orElse(null);
+    accountService.delete(account);
+    return new ResponseEntity<>("Account successfully deleted", HttpStatus.NO_CONTENT);
+  }
+}
