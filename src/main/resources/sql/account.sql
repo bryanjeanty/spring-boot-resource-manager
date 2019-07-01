d1a0a9f0e802ee9c9afee27edbb119c861b4a74a
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-07-01 09:44:04 -0400

    tables setup

--- src/main/resources/sql/account.sql
+++ src/main/resources/sql/account.sql
@@ -1,9 +1,9 @@
 CREATE TABLE account
 (
-    Id INT IDENTITY(1,1),
+    Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     Username VARCHAR(50) NOT NULL,
     Email VARCHAR(100) NOT NULL,
     Password VARCHAR(255) NOT NULL,
-    CreateAt DATETIME DEFAULT(getdate()),
+    CreatedAt DATETIME DEFAULT(getdate()),
     UpdatedAt DATETIME DEFAULT(getdate())
 );
---
+++ src/main/resources/sql/dataTables.sql
@@ -0,0 +1,6 @@
+CREATE TABLE dataTables
+(
+	Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	ReferenceTableVersionNumber INT NOT NULL,
+	DerivedTableVersionNumber INT NOT NULL
+);
