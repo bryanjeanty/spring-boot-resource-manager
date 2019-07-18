672d4e79c0e96cfaa45e132f8ace5d0ad42df57a
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-07-18 15:43:50 -0400

    authentication error handling implemented

--- src/main/java/com/resource/manager/resource/controller/LoginController.java
+++ src/main/java/com/resource/manager/resource/controller/LoginController.java
@@ -1,16 +1,44 @@
 package com.resource.manager.resource.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import java.util.Map;
+import java.util.LinkedHashMap;
+
+import java.text.SimpleDateFormat;  
+import java.util.Date;  
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.servlet.HandlerMapping;
+
 @RestController
-@RequestMapping
+@RequestMapping("/api/v1/login")
 public class LoginController {
-    @PostMapping("/api/v1/login")
+	
+    @GetMapping
     @ResponseBody
-    public String handler() {
-        return "logged in";
+    public ResponseEntity<Map<String, String>> authenticationFailure(HttpServletRequest request) {
+    	Map<String, String> errorResponse = new LinkedHashMap<String, String>();
+    	
+    	SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");  
+    	Date date = new Date();  
+    	
+    	String timeStamp = (String) formatter.format(date);
+    	String urlPath = (String) request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+    	String errorMsg = "Login failed! Sorry, try again";
+    	
+    	errorResponse.put("timestamp", timeStamp);
+    	errorResponse.put("status", "401");
+    	errorResponse.put("error", "Unauthorized");
+    	errorResponse.put("message", errorMsg);
+    	errorResponse.put("path", urlPath);
+    	
+        return new ResponseEntity<Map<String, String>>(errorResponse, HttpStatus.UNAUTHORIZED);
     }
 }
